835d17a71e0435636619e8435fed47573d2346da
leopoldfreeman <leopold.freeman.0816@gmail.com>
2015-08-25 19:30:10 +0800

    Wrap ssl.

--- base/security/inc/security.h
+++ base/security/inc/security.h
@@ -12,14 +12,52 @@ namespace parrot
         Security& operator=(const Security &s) = delete;
         
       public:
+        // init
+        // 
+        // Register ssl callbacks, init ssl library, ets.
         static void init();
+
+        // freeThreadErrQueue
+        // 
+        // Remove error queue of thread. When stop a thread, this
+        // funciton should be called.
+        //
+        // Param:
+        // * id     The id of the target thread.
         static void freeThreadErrQueue(const std::thread::id &id);
-        static void deinit();
+
+        // genSslCtx
+        // 
+        // Create a ssl contex.
+        // 
+        // Param:
+        // * keyPath     The absolute file path of key file.
+        // * certPath    The absolute file path of cert file.
+        // * verifyPeer  Verify peer if true.
+        // * depth       The verify depth.
         static ssl_ctx* genSslCtx(const string &keyPath, 
                                   const string &certPath, 
-                                  bool verifyPeer = false);
+                                  bool verifyPeer = false,
+                                  int depth = 1);
 
-        
+        // checkConnection
+        // 
+        // For server, if you want to verify client cert, call this 
+        // function after accept. For client to verify server, call
+        // this function after connect.
+        // 
+        // Param:
+        // * ssl         The ssl of the connection.       
+        // * host        The name of host.
+        //
+        // Return:
+        //   True if successful.
+        static bool checkConnection(SSL *ssl, const string &host);
+
+        // deinit
+        //
+        // Free ssl memeory.
+        static void deinit();
     };
 }
 
--- base/security/src/security.cpp
+++ base/security/src/security.cpp
@@ -1,5 +1,3 @@
-#include "security.h"
-
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
@@ -8,6 +6,10 @@
 #include <mutex>
 #include <set>
 #include <functional>
+#include <cstring>
+
+#include "security.h"
+#include "macroFuncs.h"
 
 ////////////////////////////////////////////////////////////////////////////
 /// Static locking.
@@ -48,7 +50,7 @@ static struct CRYPTO_dynlock_value * dynCreateFunctionCallback(
 }
 
 static void dynLockFunctionCallback(int mode, struct CRYPTO_dynlock_value *l,
-                              const char */*file*/, int /*line*/)
+                                    const char */*file*/, int /*line*/)
 {
     if (mode & CRYPTO_LOCK)
     {
@@ -61,11 +63,120 @@ static void dynLockFunctionCallback(int mode, struct CRYPTO_dynlock_value *l,
 }
 
 static void dynDestroyFunctionCallback(struct CRYPTO_dynlock_value *l,
-                                 const char */*file*/, int /*line*/)
+                                       const char */*file*/, int /*line*/)
 {
     delete l;
 }
 
+////////////////////////////////////////////////////////////////////////////
+/// Check host name.
+//////////////
+
+// Tries to find a match for hostname in the certificate's Common Name field.
+//
+// Tries to find a match for hostname in the certificate's 
+// Subject Alternative Name extension.
+static bool matchesCommonName(const string &host, const X509 *cert)
+{
+    int commonNameLoc = -1;
+    X509_NAME_ENTRY *commonNameEntry = nullptr;
+    ASN1_STRING *commonNameAsn1 = nullptr;
+    char *commonNameStr = nullptr;
+
+    // Find the position of the CN field in the Subject field of the certificate
+    commonNameLoc = X509_NAME_get_index_by_NID(
+        X509_get_subject_name((X509 *) cert), NID_commonName, -1);
+    if (commonNameLoc < 0) 
+    {
+        return false;
+    }
+
+    // Extract the CN field
+    commonNameEntry = X509_NAME_get_entry(
+        X509_get_subject_name((X509 *) cert), commonNameLoc);
+    if (commonNameEntry == nullptr) 
+    {
+        return false;
+    }
+
+    // Convert the CN field to a C string
+    commonNameAsn1 = X509_NAME_ENTRY_get_data(commonNameEntry);
+    if (commonNameAsn1 == nullptr) 
+    {
+        return false;
+    }
+    commonNameStr = (char *) ASN1_STRING_data(commonNameAsn1);
+
+    // Make sure there isn't an embedded NUL character in the CN
+    if (ASN1_STRING_length(commonNameAsn1) != strlen(commonNameStr)) 
+    {
+        return false;
+    }
+
+    // Compare expected hostname with the CN
+    if (std::strcmp(host.c_str(), commonNameStr) == 0) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+// matchesSubjectAlternativeName
+//
+// Tries to find a match for hostname in the certificate's 
+// Subject Alternative Name extension.
+static bool matchesSubjectAlternativeName(const string &host, const X509 *cert)
+{
+    int sanNamesNb = -1;
+    STACK_OF(GENERAL_NAME) *sanNames = nullptr;
+
+    // Try to extract the names within the SAN extension from the certificate
+    sanNames = X509_get_ext_d2i((X509 *) cert, NID_subject_alt_name, 
+                                nullptr, nullptr);
+    if (sanNames == nullptr) 
+    {
+        return false;
+    }
+    sanNamesNb = sk_GENERAL_NAME_num(sanNames);
+
+    bool res = false;
+
+    // Check each name within the extension
+    for (int i = 0; i < sanNamesNb; ++i) 
+    {
+        const GENERAL_NAME *currentName = sk_GENERAL_NAME_value(sanNames, i);
+
+        if (currentName->type == GEN_DNS) 
+        {
+            // Current name is a DNS name, let's check it
+            char *dnsName = (char *) ASN1_STRING_data(currentName->d.dNSName);
+
+            // Make sure there isn't an embedded NUL character in the DNS name
+            if (ASN1_STRING_length(currentName->d.dNSName) != strlen(dnsName))
+            {
+                res = false;
+                break;
+            }
+            else 
+            {
+                // Compare expected hostname with the DNS name
+                if (std::strcmp(host.c_str(), dnsName) == 0) 
+                {
+                    res = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    sk_GENERAL_NAME_pop_free(sanNames, GENERAL_NAME_free);
+    return res;
+}
+
+//////////////////////////////////////////////////////////////////////////////
 namespace parrot
 {
     Security::Security() = default;
@@ -105,6 +216,80 @@ namespace parrot
         std::hash<std::thread::id> hasher;
         tid.val = (unsigned long)hasher(id);
         ERR_remove_thread_state(&tid);
+    }
+
+    SSL_CTX* Security::genSslCtx(const string &keyPath, 
+                                 const string &certPath, 
+                                 bool verifyPeer,
+                                 int depth)
+    {
+        const SSL_METHOD *m = TLSv1_method();
+        SSL_CTX *sslCtx = SSL_CTX_new(m);
+
+        if (!sslCtx)
+        {
+            // If here, did you forget to call Security::init()?
+            PARROT_ASSERT(0);
+        }
+
+        if (SSL_CTX_load_verify_locations(sslCtx, certPath, nullptr) != 1)
+        {
+            PARROT_ASSERT(0);
+        }
+
+        if (SSL_CTX_set_default_verify_paths(sslCtx) != 1)
+        {
+            PARROT_ASSERT(0);
+        }
+
+        if (SSL_use_certificate_chain_file(sslCtx, certPath.c_str()) != 1)
+        {
+            PARROT_ASSERT(0);
+        }
+
+        if (SSL_CTX_use_PrivateKey_file(sslCtx, keyPath.c_str(), 
+                                        SSL_FILETYPE_PEM) != 1)
+        {
+            PARROT_ASSERT(0);
+        }
+
+        if (verifyPeer) 
+        {
+            SSL_CTX_set_verify(
+                sslCtx, 
+                SSL_VERIFY_PEER|SSL_VERIFY_FAIL_IF_NO_PEER_CERT, 
+                nullptr);
+            SSL_CTX_set_verify_depth(sslCtx, depth);
+        }
+        else
+        {
+            SSL_CTX_set_verify(sslCtx, SSL_VERIFY_NONE, nullptr);
+        }
+
+        return sslCtx;
+    }
+
+    bool checkConnection(SSL *ssl, const string &host)
+    {
+        X509 *cert = nullptr;
+        if (SSL_get_verify_result(ssl) != X509_V_OK)
+        {
+            return false;
+        }
+
+        if (!(cert = SSL_get_peer_certificate(ssl)) || host.empty())
+        {
+            return false;
+        }
+
+        if (!matchesCommonName(host, cert) || 
+            !matchesSubjectAlternativeName(host, cert)) 
+        {
+            return false;
+        }
+
+        X509_free(cert);
+        return true;
     }
 
     void Security::deinit()
