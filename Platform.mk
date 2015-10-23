3a3a91868fe2d1c8470943c2f9d452731142ce12
leopoldfreeman <leopold.freeman.0816@gmail.com>
2015-10-23 21:32:21 +0800

    Indent file

--- Platform.mk
+++ Platform.mk
@@ -1,23 +1,23 @@
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 uname_M := $(shell sh -c 'uname -m 2>/dev/null || echo not')
 
-THIRD_PARTY_DIR := $(PRJ_ROOT)/third-party
+THIRD_PARTY_DIR   := $(PRJ_ROOT)/third-party
 
 ifeq ($(uname_S), mingw64_win)
 
 # Rapidjson
-CPPFLAGS      += -DRAPIDJSON_HAS_STDSTRING -DRAPIDJSON_SSE2 -DRAPIDJSON_SSE42
+CPPFLAGS          += -DRAPIDJSON_HAS_STDSTRING -DRAPIDJSON_SSE2 -DRAPIDJSON_SSE42
 endif
 
 ifeq ($(uname_S), Linux)
 
 # Openssl
-OPENSSL_INC := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/openssl/include
-OPENSSL_LIB := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/openssl/lib
+OPENSSL_INC       := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/openssl/include
+OPENSSL_LIB       := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/openssl/lib
 
 # Rapidjson
-RAPIDJSON_INC := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/rapidjson/include
-CPPFLAGS      += -DRAPIDJSON_HAS_STDSTRING -DRAPIDJSON_SSE2
+RAPIDJSON_INC     := $(THIRD_PARTY_DIR)/$(uname_S).$(uname_M)/rapidjson/include
+CPPFLAGS          += -DRAPIDJSON_HAS_STDSTRING -DRAPIDJSON_SSE2
 endif
 
 ifeq ($(uname_S), Darwin)
